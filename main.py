eb82177f4f58acb01b47e2c88ac8c581f5def0e7
Rosario Lourenco <rosariomassango@gmail.com>
2023-09-11 00:39:09 +0100

    Feat: bullet shooot

--- main.py
+++ main.py
@@ -3,31 +3,45 @@ import pygame
 import src.player as player
 import src.enemy as enemy
 
-from src.config import config
+import src.utils as utils
 
 # initialize game
 pygame.init()
 
 pygame.display.set_caption("Space Invasion")
-screen = pygame.display.set_mode((config.screen_with, config.screen_height))
+screen = pygame.display.set_mode((utils.screen_with, utils.screen_height))
+background = pygame.image.load("./assets/background.jpg")
 
 running = True
 while running:
-   screen.fill("purple")
+   #Background
+   screen.blit(background, (0,0))
 
    for event in pygame.event.get():
+
+      #Closing Event
       if event.type == pygame.QUIT:
          running = False
 
+      # Process key event
       if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-         player.x_change = player.move(event)
+         player.x_axis_change = player.move(event)
 
-   #Change player location
-   player.x += player.x_change
-   player.handler(screen, player.x)
+      if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+         player.shoot(screen, player.x_axis, player.bullet_y_axis)
 
-   #Enemies
-   enemy.x += enemy.x_change
-   enemy.handler(screen, enemy.x)
+
+   #Change player and enemy Location
+   player.x_axis += player.x_axis_change
+   enemy.x_axis += enemy.x_axis_change
+
+   #Bullet Moviment
+   if player.visible_bullet:
+      player.shoot(screen, player.x_axis, player.bullet_y_axis)
+      player.bullet_y_axis -= player.bullet_y_axis_change
+
+   #Handler players and enemies
+   player.handler(screen, player.x_axis)
+   enemy.handler(screen, enemy.x_axis)
 
    pygame.display.update()
